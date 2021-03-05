ce13deb912532e4d7f306bcfdc7a30bbcdac7c7d
dvkr <v3dvkr@gmail.com>
2021-03-04 19:07:09 -0700

    added today's kata solution

--- March.js
+++ March.js
@@ -1,3 +1,19 @@
+// Is this my tail? https://www.codewars.com/kata/56f695399400f5d9ef000af5
+
+function correctTail(bod, tail){
+  
+  sub = bod.substr(bod.length-(tail.length))
+  
+  if (sub == tail) {
+    return true
+  }
+  else {
+    return false
+  }
+
+}
+
+
 // Total amount of points https://www.codewars.com/kata/5bb904724c47249b10000131
 
 function points(games) {
