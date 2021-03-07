74fcb93c1d1d967e69e3c84cd1f14ad33a85cf74
dvkr <v3dvkr@gmail.com>
2021-03-06 19:09:39 -0700

    added today's kata solution

--- March.js
+++ March.js
@@ -1,3 +1,9 @@
+// Remove duplicates from list https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
+
+function distinct(a) {
+  return Array.from(new Set(a));
+}
+
 // Sleigh Authentication https://www.codewars.com/kata/52adc142b2651f25a8000643
 
 function Sleigh() {}
