9c0371f627c2949ab1ff0213ee57fea190342fb8
dvkr <v3dvkr@gmail.com>
2021-04-19 20:55:54 -0600

    added string kata 2

--- April.js
+++ April.js
@@ -3,3 +3,9 @@
 function greet(name){
     return `Hello, ${name} how are you doing today?`;
   }
+
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
