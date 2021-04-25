dc9859d07a649c3eca5266073bd94f75686054ff
dvkr <v3dvkr@gmail.com>
2021-04-24 18:00:48 -0600

    added array kata 1

--- April.js
+++ April.js
@@ -1,3 +1,4 @@
+// Strings:  -------------------------------
 // https://www.codewars.com/kata/55a70521798b14d4750000a4
 
 function greet(name){
@@ -33,4 +34,10 @@ function getCount(str) {
 
 function disemvowel(str) {
   return str.split('').filter(x => !'aeiou'.includes(x.toLowerCase())).join('');
+}
+
+// Arrays: ---------------------------------
+// https://www.codewars.com/kata/523f5d21c841566fde000009
+function arrayDiff(a, b) {
+  return a.filter(x => !b.includes(x))
 }
